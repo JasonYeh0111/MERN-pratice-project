df49855820fcef9cf4c7642e7d5f824679e92d4d
Jason Yeh <jason30307@gmail.com>
2024-03-18 14:40:55 +0800

    axios

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/user";
+const API_URL = "https://mern-pratice-project.onrender.com/api/user";
 
 class AuthService {
   login(email, password) {
--- client/src/services/course.service.js
+++ client/src/services/course.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/courses";
+const API_URL = "https://mern-pratice-project.onrender.com/api/courses";
 
 class CourseService {
   post(title, description, price) {
